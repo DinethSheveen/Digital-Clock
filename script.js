021eab8163299efcfe44e50d46e8408b4ae0c2a8
DinethSheveen <fdineth4000@gmail.com>
2025-08-20 17:42:55 +0100

    Added a meridiem to the clock

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const timeElement = document.querySelector(".clock")
 
 function updateTime(){
     let present = new Date();
-    
     let hours = present.getHours()
     let minutes = present.getMinutes().toString().padStart(2,0)
     let seconds = present.getSeconds().toString().padStart(2,0)
 
-    timeElement.innerHTML = `${hours}:${minutes}:${seconds} `
+    let meridiem = hours >= 12 ? "PM" : "AM"
+
+    timeElement.innerHTML = `${hours}:${minutes}:${seconds} ${meridiem}`
 }
 
 updateTime()
